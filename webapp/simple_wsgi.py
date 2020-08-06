984db707ad4785c5857f12e10e27005ef99fe6cd
NiShadrin <nishadrin@gmail.com>
2020-08-06 20:14:00 +0300

    post in views

--- webapp/simple_wsgi.py
+++ webapp/simple_wsgi.py
@@ -1,3 +1,5 @@
+import urllib.parse
+
 from templator import render
 from views import ViewRequests
 from routes import routes
@@ -5,7 +7,7 @@ from middleware import slash_endswith
 
 
 middlewares = [slash_endswith]
-views_request = ViewRequests()
+view_request = ViewRequests()
 
 
 class Application:
@@ -13,7 +15,7 @@ class Application:
     def __init__(self, routes, middlewares):
         self.routes = routes
         self.middlewares = middlewares
-        self.view = views_request.not_found_404_view
+        self.view = view_request.not_found_404_view
 
     def __call__(self, environ, start_response):
         """
@@ -28,16 +30,36 @@ class Application:
             self.view = self.routes[environ.get('PATH_INFO')]
 
         if environ.get('REQUEST_METHOD') == "POST":
-            # self.post_request(environ)
-            self.view = views_request.post_request(environ)
+            self.post_request(environ)
 
         code, body = self.view(environ)
-
         body = [body.encode()]
 
         start_response(code, [('Content-Type', 'text/html')])
 
         return body
+
+    def post_request(self, environ):
+        data = self.get_wsgi_data(environ)
+        data = self.parse_wsgi_data(data)
+        environ['parsing_wsgi_data'] = data
+
+    def get_wsgi_data(self, env) -> bytes:
+        if env.get('CONTENT_LENGTH'):
+            return env['wsgi.input'].read(int(env['CONTENT_LENGTH'])) \
+                    if int(env['CONTENT_LENGTH']) > 0 else b''
+
+    def parse_wsgi_data(self, data: bytes) -> dict:
+        result = {}
+
+        if data:
+            data = urllib.parse.unquote(data.decode(encoding='utf-8'))
+            query = data.split('&')
+            for item in query:
+                i, j = item.split('=')
+                result[i] = j
+
+        return result
 
 
 application = Application(routes, middlewares)
--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,4 @@
 import os
-import urllib.parse
 
 from templator import render
 
@@ -9,35 +8,6 @@ path = os.getcwd()
 
 class ViewRequests:
     """docstring for ViewRequests."""
-
-    def post_request(self, environ):
-        data = self.get_wsgi_data(environ)
-        data = self.parse_wsgi_data(data)
-        environ['parsing_wsgi_data'] = data
-        print(data)
-
-        if data:
-            return self.feed_back_email
-
-        return self.not_found_404_view
-
-    def get_wsgi_data(self, env) -> bytes:
-        if env.get('CONTENT_LENGTH'):
-            return env['wsgi.input'].read(int(env['CONTENT_LENGTH'])) \
-                    if int(env['CONTENT_LENGTH']) > 0 else b''
-
-    def parse_wsgi_data(self, data: bytes) -> dict:
-        result = {}
-
-        if data:
-            data = urllib.parse.unquote(data.decode(encoding='utf-8'))
-            query = data.split('&')
-            for item in query:
-                i, j = item.split('=')
-                result[i] = j
-
-        return result
-
 
     def not_found_404_view(self, request):
         return '404 Not Found', [b' Error 404 page not Found']
@@ -50,9 +20,8 @@ class ViewRequests:
     def contact_view(self, request):
         page = render(f'{path}/contact.html')
 
-        return '200 OK', page
-
-    def feed_back_email(self, request):
-        page = render(f'{path}/feed_back.html', data={'email': request['parsing_wsgi_data']['email']})
+        if request['REQUEST_METHOD'] == "POST":
+            print(request['parsing_wsgi_data'])
+            page = render(f'{path}/feed_back.html', data={'email': request['parsing_wsgi_data']['email']})
 
         return '200 OK', page

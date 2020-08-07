8e23d2521a3fc27cf7a1e138f2516cebdf40e760
NiShadrin <nishadrin@gmail.com>
2020-08-07 17:30:06 +0300

    refactoring with abstract pattern

--- webapp/views.py
+++ webapp/views.py
@@ -1,27 +1,60 @@
 import os
+from abc import ABC, abstractmethod
+from typing import Tuple, Dict
 
-from templator import render
+from templator import TemplateRender
+from common.http_status import StatusCreator
 
 
+http_status = StatusCreator()
+render = TemplateRender().render
 path = os.getcwd()
 
 
-class ViewRequests:
-    """docstring for ViewRequests."""
+class DefaultPage(ABC):
+    """docstring for DefaultPage."""
 
-    def not_found_404_view(self, request):
-        return '404 Not Found', [b' Error 404 page not Found']
+    def __init__(self, request: Dict):
+        self.request = request
 
-    def index_view(self, request):
-        page = render(f'{path}/index.html', course_list=[{'name': 'Python'}, {'name': 'Java'}, {'name': 'PHP'}, {'name': 'JS'}])
+    @abstractmethod
+    def render(self) -> Tuple[str]:
+        pass
 
-        return '200 OK', page
 
-    def contact_view(self, request):
-        page = render(f'{path}/contact.html')
+class NotFound404(DefaultPage):
+    """docstring for NotFound404."""
 
-        if request['REQUEST_METHOD'] == "POST":
-            print(request['parsing_wsgi_data'])
-            page = render(f'{path}/feed_back.html', data={'email': request['parsing_wsgi_data']['email']})
+    def __init__(self, request: Dict):
+        super().__init__(request)
 
-        return '200 OK', page
+    def render(self) -> Tuple[str]:
+        return http_status(404), ' Error 404 page not Found'
+
+
+class Index(DefaultPage):
+    """docstring for Index."""
+
+    def __init__(self, request: Dict):
+        super().__init__(request)
+
+    def render(self) -> Tuple[str]:
+        page = render('index.html', course_list=[{'name': 'Python'}, {'name': 'Java'}, {'name': 'PHP'}, {'name': 'JS'}])
+
+        return http_status(), page
+
+
+class Contact(DefaultPage):
+    """docstring for Contact."""
+
+    def __init__(self, request: Dict):
+        super().__init__(request)
+
+    def render(self) -> Tuple[str]:
+        page = render('contact.html')
+
+        if self.request['REQUEST_METHOD'] == "POST":
+            print(self.request['parsing_wsgi_data'])
+            page = render('feed_back.html', data={'email': self.request['parsing_wsgi_data']['email']})
+
+        return http_status(), page

83c340b1fa64dec4c20bdda146359c8d400732c7
NiShadrin <nishadrin@gmail.com>
2020-08-07 17:57:15 +0300

    delete init in views page

--- webapp/views.py
+++ webapp/views.py
@@ -25,18 +25,12 @@ class DefaultPage(ABC):
 class NotFound404(DefaultPage):
     """docstring for NotFound404."""
 
-    def __init__(self, request: Dict):
-        super().__init__(request)
-
     def render(self) -> Tuple[str]:
         return http_status(404), ' Error 404 page not Found'
 
 
 class Index(DefaultPage):
     """docstring for Index."""
-
-    def __init__(self, request: Dict):
-        super().__init__(request)
 
     def render(self) -> Tuple[str]:
         page = render('index.html', course_list=[{'name': 'Python'}, {'name': 'Java'}, {'name': 'PHP'}, {'name': 'JS'}])
@@ -46,9 +40,6 @@ class Index(DefaultPage):
 
 class Contact(DefaultPage):
     """docstring for Contact."""
-
-    def __init__(self, request: Dict):
-        super().__init__(request)
 
     def render(self) -> Tuple[str]:
         page = render('contact.html')

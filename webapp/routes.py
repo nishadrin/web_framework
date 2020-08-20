f21cea51097fc0efe2b0bd036c3fe1e339138e7c
NiShadrin <nishadrin@gmail.com>
2020-08-20 19:12:30 +0300

    add routes for courses and categories html

--- webapp/routes.py
+++ webapp/routes.py
@@ -1,7 +1,12 @@
-from views import Contact, Index
+from views import Contact, Index, CourseCreate, CourseList, CategoryCreate, CategoryList, CopyCourse
 
 
 routes = {
     '/': Index,
     '/contact/': Contact,
+    '/course-create/': CourseCreate,
+    '/course-list/': CourseList,
+    '/category-create/': CategoryCreate,
+    '/category-list/': CategoryList,
+    '/copy-course/': CopyCourse,
 }

8e24dce7ab5b4755f60c844af4ed3ae7ec73dede
NiShadrin <nishadrin@gmail.com>
2020-09-01 20:10:39 +0300

    add courses list in category

--- webapp/education/models/category.py
+++ webapp/education/models/category.py
@@ -1,7 +1,12 @@
+from typing import List, Optional
 
+from course import Course
 
 class Category:
     """docstring for Category."""
+
+    def __init__(self):
+        self.__courses = list()
 
     @property
     def name(self) -> str:
@@ -9,3 +14,17 @@ class Category:
 
     def set_name(self, name: str):
         self.__name = name
+
+    def add_course(self, course: Course):
+        self.__courses.append(course)
+
+    def remove_course(self, course: Course):
+        try:
+            self.__courses.remove(course)
+        except ValueError as e:
+            print(f"We haven't {course.name} in {self.__name} categories.")
+            raise
+
+    @property
+    def courses(self) -> List[Optional]:
+        return self.__courses
